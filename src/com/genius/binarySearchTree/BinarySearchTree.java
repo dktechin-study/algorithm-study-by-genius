fc5c2ee1d03792610b9ed0a68b6f007f5d4276b2
genius.choi <genius.choi@kakaocorp.com>
2017-01-24 02:28:34 +0900

    BinarySearchTree

---
+++ src/com/genius/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,46 @@
+package com.genius.binarySearchTree;
+
+public class BinarySearhTree {
+
+	private Node root;
+
+	public void add(int value) {
+		Node temp = root;
+		Node node = new Node(value);
+		if (root == null) root = node;
+		while (temp != null) {
+			if (temp.value == value) return;
+			if (temp.value > value) {
+				if (temp.left == null) {
+					temp.left = node;
+				} else {
+					temp = temp.left;
+				}
+			} else {
+				if (temp.right == null) {
+					temp.right = node;
+				} else {
+					temp = temp.right;
+				}
+			}
+		}
+	}
+
+	public void delete(int value) {
+
+	}
+
+	public Node search(int value) {
+		Node temp = root;
+		while (temp != null) {
+			if (temp.value == value) return temp;
+			if (temp.value > value) {
+				temp = temp.left;
+			} else {
+				temp = temp.right;
+			}
+		}
+		return null;
+	}
+}
+
---
+++ src/com/genius/binarySearchTree/Node.java
@@ -0,0 +1,7 @@
+package com.genius.binarySearchTree;
+
+/**
+ * Created by Genius on 2017-01-24.
+ */
+public class Node {
+}
