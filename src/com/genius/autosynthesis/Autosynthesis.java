c63be202c773ea65a53785733ab41c59395aff1f
genius.choi <genius.choi@kakaocorp.com>
2017-02-08 10:36:39 +0900

    세포분열 재도전 2 하드코딩 :)

--- src/com/genius/autosynthesis/Autosynthesis.java
+++ src/com/genius/autosynthesis/Autosynthesis.java
@@ -36,12 +36,26 @@ public class Autosynthesis {
 		System.out.println(Arrays.toString(c));
 
 		int R = -1;
-		d[n - 2] = c[n - 1];
-		d[1] = c[2] - d[3];
+		boolean continues = true;
 
+		while (continues) {
+			d[n - 2] = c[n - 1];
+			d[1] = c[2] - d[3];
 
-		System.out.println(Arrays.toString(d));
+			d[0] = c[0] - d[1];
 
+			d[2] = c[1] - d[0];
+			d[4] = c[3] - d[2];
+
+			R++;
+			System.out.println(Arrays.toString(d));
+
+			c = Arrays.copyOf(d, d.length);
+
+			if (Arrays.stream(c).anyMatch(i -> i < 0)) {
+				continues = false;
+			}
+		}
 		System.out.println("==================");
 		System.out.println(R + " 회 분할");
 	}
