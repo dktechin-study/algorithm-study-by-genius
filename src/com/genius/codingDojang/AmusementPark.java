36b2adbbe74670dae1f31ab22d289fee206f3053
genius.choi <genius.choi@kakaocorp.com>
2017-02-11 22:15:00 +0900

    놀이공원 인형 맞추기 끝

--- src/com/genius/codingDojang/AmusementPark.java
+++ src/com/genius/codingDojang/AmusementPark.java
@@ -3,7 +3,8 @@ package com.genius.codingDojang;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AmusementPark {
@@ -13,13 +14,25 @@ public class AmusementPark {
 		Scanner sc = new Scanner(new File(path + "AmusementPark.txt"));
 
 		int size = sc.nextInt();
-		int targetSum = sc.nextInt();
+		int target = sc.nextInt();
 		int[] dolls = new int[size];
 
 		for (int i = 0; i < size; i++) {
 			dolls[i] = sc.nextInt();
 		}
-		Arrays.sort(dolls);
-		System.out.println(Arrays.toString(dolls));
+
+		for (int i = 1; i < 1 << size; i++) {
+			int k = i;
+			int d = 0;
+			List<Integer> t = new ArrayList<>();
+			while (k > 0) {
+				if (k % 2 == 1) t.add(dolls[d]);
+				k = k / 2;
+				d++;
+			}
+			if (t.stream().mapToInt(Integer::intValue).sum() == target) {
+				System.out.println(t);
+			}
+		}
 	}
 }
