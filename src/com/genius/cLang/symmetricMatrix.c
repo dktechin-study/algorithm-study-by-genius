2e8e0fe03d985f051d096042ff4bde7c8671831f
genius.choi <genius.choi@kakaocorp.com>
2017-02-28 02:41:47 +0900

    subsetsum

--- src/com/genius/cLang/symmetricMatrix.c
+++ src/com/genius/cLang/symmetricMatrix.c
@@ -3,31 +3,30 @@ char tmpChar[2];
 char initMatrix[128];
 
 int main(void) {
-    int tc;
-    int i, j;
-    int testCases;
-    int num_Matrix;
-    
-    freopen("symmetricMatrix.txt", "r", stdin);
+	int tc;
+	int i, j;
+	int testCases;
+	int num_Matrix;
 
-    scanf("%d", &testCases);
-    
-    for(tc = 0; tc < testCases; tc++) {
-        scanf("%d", &num_Matrix);        
-	for(i = 0; i < num_Matrix; i++) {
-            scanf("%s", &initMatrix[i]);
-        }
-        for(i = 0; i < num_Matrix; i++) {
-            for(j=0; j < num_Matrix; j++) {
-                char c = initMatrix[i ^ j];
-		if(j == num_Matrix -1)
-                    printf("%c\n", c);
-                else 
-                    printf("%c", c);
-            }
-        }
-        printf("\n");
-    }
+	freopen("symmetricMatrix.txt", "r", stdin);
 
-    return 0;	
+	scanf("%d", &testCases);
+
+	for(tc = 0; tc < testCases; tc++) {
+		scanf("%d", &num_Matrix);        
+		for(i = 0; i < num_Matrix; i++) {
+			scanf("%s", &initMatrix[i]);
+		}
+		for(i = 0; i < num_Matrix; i++) {
+			for(j=0; j < num_Matrix; j++) {
+				char c = initMatrix[i ^ j];
+				if(j == num_Matrix -1)
+				printf("%c\n", c);
+				else 
+				printf("%c", c);
+				}
+			}
+			printf("\n");
+		}
+	return 0;	
 }
