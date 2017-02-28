260fd8e73cf4db11f8185a357d61ecc6bdb6e01c
genius.choi <genius.choi@kakaocorp.com>
2017-02-28 12:52:39 +0900

    VehicleArrangement

--- src/com/genius/vehicleArrangement/VehicleArrangement.java
+++ src/com/genius/vehicleArrangement/VehicleArrangement.java
@@ -1,21 +1,37 @@
 package com.genius.vehicleArrangement;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
-/**
- * Created by genius on 2017. 2. 27..
- */
 public class VehicleArrangement {
 
-	public static void main(String[] args) {
-		Integer[] heights = new Integer[]{75, 30, 100, 38, 50};
-		List<Integer> list = new ArrayList<>();
+	public static void main(String[] args) throws FileNotFoundException {
 
-		IntStream.range(0, 5).forEach(i -> {list.add(heights[i]);});
+		Scanner sc = new Scanner(new File(VehicleArrangement.class.getResource("").getPath() + "VehicleArrangement.txt"));
 
-		System.out.println(list.stream().min(Integer::compareTo).get());
+		int n = sc.nextInt();
 
+		for (int i = 0; i < n; i++) {
+			int m = sc.nextInt();
+			int l = sc.nextInt();
+
+			Integer[] heights = new Integer[m];
+
+			for (int j = 0; j < m; j++) {
+				heights[j] = sc.nextInt();
+			}
+
+			for (int k = 0; k < l; k++) {
+				List<Integer> list = new ArrayList<>();
+				IntStream.range(sc.nextInt(), sc.nextInt()).forEach(t -> list.add(heights[t]));
+				System.out.println(list.stream().min(Integer::compareTo).orElse(0));
+			}
+
+			System.out.println("");
+		}
 	}
 }
