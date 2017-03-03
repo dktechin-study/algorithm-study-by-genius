ba7fd6dbd6a1a6577ca34cc481624f034567dcbc
genius.choi <genius.choi@kakaocorp.com>
2017-03-04 03:54:32 +0900

    Fibonacci2

--- src/com/genius/Algorithm.java
+++ src/com/genius/Algorithm.java
@@ -2,54 +2,12 @@ package com.genius;
 
 import java.util.Scanner;
 
-/**
- * Created by genius on 2017. 2. 17..
- */
 public class Algorithm {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		solve(n);
-		solve2(n);
-	}
-
-	private static void solve2(int n) {
-		for (int i = 0; i < 1666; i++) {
-			for (int j = 0; j < 1000; j++) {
-				if ((3 * i) + (5 * j) == n) {
-					System.out.println("3 = " + i + ", 5 = " + j);
-					return;
-				}
-			}
+		int n = sc.nextInt();		
+		for(int i = 1; i < 10; i++){
+			System.out.printf("%d * %d = %d", n, i, i*n);
 		}
-		System.out.println("-1");
-	}
-
-	private static void solve(int n) {
-		if (n % 5 == 0) {
-			System.out.println(n / 5);
-			return;
-		}
-
-		if ((n % 5) % 3 == 0) {
-			System.out.println((n / 5) + (n % 5) / 3);
-			return;
-		}
-
-		int k = (n / 5) - 1;
-		while (k > 0) {
-			if ((n - k * 5) % 3 == 0) {
-				System.out.println(k + ((n - k * 5) / 3));
-				return;
-			}
-			k--;
-		}
-
-		if ((n % 3) == 0) {
-			System.out.println(n / 3);
-			return;
-		}
-
-		System.out.println("-1");
 	}
 }
--- src/com/genius/codingDojang/Fibonacci.java
+++ src/com/genius/codingDojang/Fibonacci.java
@@ -11,7 +11,7 @@ public class Fibonacci {
 
 	private static int fibonacci(int n) {
 		if (n == 0) return 0;
-		if (n <= 1) return 1;
+		if (n == 1) return 1;
 		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
 }
