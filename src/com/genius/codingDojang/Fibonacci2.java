b95af0f547062934afa8b679727cda820f5dc1c6
genius.choi <genius.choi@kakaocorp.com>
2017-03-05 03:14:27 +0900

    SameNumber

--- src/com/genius/codingDojang/Fibonacci2.java
+++ src/com/genius/codingDojang/Fibonacci2.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 
 public class Fibonacci2 {
 
-    static Map<Long, Long> map = new HashMap<>();
+	static Map<Long, Long> map = new HashMap<>();
 
-    public static void main(String[] args) {
-        long l = 4294967291L;
-        long m = new Scanner(System.in).nextLong();
-        for (long i = 0; i < m; i++) {
-            map.put(i + 1, fibonacci(i) % l);
-        }
-        System.out.println(map.get(m));
-    }
+	public static void main(String[] args) {
+		long l = 4294967291L;
+		long m = new Scanner(System.in).nextLong();
+		for (long i = 0; i < m; i++) {
+			map.put(i + 1, fibonacci(i) % l);
+		}
+		System.out.println(map.get(m));
+	}
 
-    private static long fibonacci(long n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (map.get(n + 1l) != null) return map.get(n + 1l);
-        return fibonacci(n - 2) + fibonacci(n - 1);
-    }
+	private static long fibonacci(long n) {
+		if (n == 0) return 0;
+		if (n == 1) return 1;
+		if (map.get(n + 1l) != null) return map.get(n + 1l);
+		return fibonacci(n - 2) + fibonacci(n - 1);
+	}
 }
---
+++ src/com/genius/codingDojang/SameNumber.java
@@ -0,0 +1,59 @@
+package com.genius.codingDojang;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class SameNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		BigDecimal n = sc.nextBigDecimal();
+		BigDecimal t = n;
+		BigDecimal r = BigDecimal.ZERO;
+		while (true) {
+			t = t.divide(BigDecimal.TEN);
+			if (t.compareTo(BigDecimal.ONE) == -1) {
+				break;
+			}
+			r = r.add(BigDecimal.ONE);
+		}
+
+		BigDecimal a = pow(r);
+		BigDecimal c = n.subtract(a);
+		if (c.compareTo(a) == -1 && r.compareTo(BigDecimal.ONE) == 1) {
+			c = n.subtract(a.divide(BigDecimal.TEN));
+		}
+		String d = String.valueOf(c);
+		StringBuffer e = new StringBuffer(d.substring(0, d.length() - (c.compareTo(BigDecimal.valueOf(2)) == 1 ? 1 : 0)));
+		System.out.println(d + "" + e.reverse());
+	}
+
+
+	private static BigDecimal pow(BigDecimal r) {
+		BigDecimal c = BigDecimal.ZERO;
+		BigDecimal d = BigDecimal.ONE;
+		while (c.compareTo(r) == -1) {
+			d = d.multiply(BigDecimal.TEN);
+			c = c.add(BigDecimal.ONE);
+		}
+		return d;
+	}
+}
+
+//		BigDecimal n = sc.nextBigDecimal();
+//		BigDecimal i = BigDecimal.ZERO;
+//		BigDecimal c = BigDecimal.ZERO;
+//
+//		while (true) {
+//			StringBuffer s = new StringBuffer(i.toString());
+//			StringBuffer sb = new StringBuffer(i.toString());
+//			if (s.toString().equals(sb.reverse().toString())) {
+//				c = c.add(BigDecimal.ONE);
+//				if (c.compareTo(n) == 0) {
+//					System.out.println("Same : " + sb);
+//					break;
+//				}
+//			}
+//			i = i.add(BigDecimal.ONE);
+//		}
