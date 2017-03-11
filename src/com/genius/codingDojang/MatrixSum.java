dc777e775c5d490126f5a0eacb85f03ec9f5c162
genius.choi <genius.choi@kakaocorp.com>
2017-03-12 03:09:30 +0900

    MatrixSum2

--- src/com/genius/codingDojang/MatrixSum.java
+++ src/com/genius/codingDojang/MatrixSum.java
@@ -8,12 +8,15 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
+import static java.lang.System.out;
+import static java.lang.System.setOut;
+
 public class MatrixSum {
 
-	private static int len = 12;
+	private static int len = 15;
 	private static int max = 0;
 	private static int[][] matrix = new int[len][len];
-	private static List<int[]> perms = new ArrayList<>();
+	//private static List<int[]> perms = new ArrayList<>();
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner sc = new Scanner(new File(MatrixSum.class.getResource("").getPath() + "MatrixSum.txt"));
@@ -22,20 +25,21 @@ public class MatrixSum {
 
 		perm(0, len, IntStream.range(0, len).toArray());
 
-		IntStream.range(0, perms.size()).forEach(i -> {
+	/*	IntStream.range(0, perms.size()).forEach(i -> {
 			int t = 0;
 			for (int j = 0; j < len; j++) {
 				t = t + matrix[j][perms.get(i)[j]];
 			}
 			if (t > max) max = t;
-		});
+		});*/
 
-		System.out.println(max);
+		out.println(max);
 	}
 
 	private static void perm(int s, int n, int[] arr) {
 		if (n == s) {
-			perms.add(Arrays.copyOf(arr, arr.length));
+			//perms.add(Arrays.copyOf(arr, arr.length));
+			max(arr);
 			return;
 		}
 
@@ -50,5 +54,17 @@ public class MatrixSum {
 		int temp = arr[i];
 		arr[i] = arr[s];
 		arr[s] = temp;
+	}
+
+	private static void max(int[] arr) {
+		int t = 0;
+		for (int i = 0; i < len; i++) {
+			t = t + matrix[i][arr[i]];
+		}
+
+		if (t > max) {
+			System.out.println(t);
+			max = t;
+		}
 	}
 }
