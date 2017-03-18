08793d58b8a06ef146edf5ea5683948b8e0fccd4
genius.choi <genius.choi@kakaocorp.com>
2017-03-19 03:47:49 +0900

    LISPCalculator  완

--- src/com/genius/codingDojang/LISPCalculator.java
+++ src/com/genius/codingDojang/LISPCalculator.java
@@ -2,46 +2,72 @@ package com.genius.codingDojang;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LISPCalculator {
 
+	static Node root = null;
+	static Node current = null;
+	static Node key = null;
+
 	public static void main(String[] args) {
-		String[] strings = "(* (+ 2 3) (- 5 3))".split("");
+		final String ss = "(* (+ 2 3) (- 5 3))";
+		final Pattern p = Pattern.compile("\\*|\\+|/|-(?!\\d)|-?\\d{1,30}|\\)");
+		final Matcher m = p.matcher(ss);
 
-		Node root = null;
-		Node temp = null;
-
-		for (int i = 0; i < strings.length; i++) {
-			final String mg = strings[i];
-			if (mg.equals("(*") || mg.equals("(/") | mg.equals("(+") | mg.equals("(-")) {
+		while (m.find()) {
+			final String mg = m.group();
+			if (mg.equals("*") || mg.equals("/") | mg.equals("+") | mg.equals("-")) {
+				Node node = new Node(mg, new ArrayList<>());
 				if (root == null) {
-					root = new Node(mg, new ArrayList<>());
-					temp = root;
-				} else if (temp.left == null) {
-					temp.left = new Node(mg, new ArrayList<>());
-				} else if (temp.right == null) {
-					temp.right = new Node(mg, new ArrayList<>());
-					temp = temp.left;
+					root = node;
+					current = node;
+					key = node;
+				} else if (current.left == null) {
+					current.left = node;
+					node.parent = current;
+					key = node;
+				} else if (current.right == null) {
+					current.right = node;
+					node.parent = current;
+					key = node;
+					current = current.left;
 				}
-			} else if(mg.equals(")")) {
-
+			} else if (mg.equals(")")) {
+				if (current.parent != null) current = current.parent;
+				key = current;
+			} else {
+				key.numbers.add(Integer.valueOf(mg));
 			}
 		}
 
 		recursive(root);
+		String o = root.operation;
+		System.out.println(root.numbers.stream().reduce((a, b) -> operation(a, b, o)).orElse(0));
 	}
 
 	private static void recursive(Node node) {
 		if (node == null) return;
 		recursive(node.left);
 		recursive(node.right);
-		System.out.println(node.operation);
+		node.result = node.numbers.stream().reduce((a, b) -> operation(a, b, node.operation)).orElse(0);
+		if (node != root) node.parent.numbers.add(node.result);
+	}
+
+	private static Integer operation(Integer a, Integer b, String o) {
+		if (o.equals("+")) return a + b;
+		if (o.equals("-")) return a - b;
+		if (o.equals("*")) return a * b;
+		if (o.equals("/")) return a / b;
+		return 0;
 	}
 }
 
 class Node {
 	Node left;
 	Node right;
+	Node parent = null;
 	String operation;
 	List<Integer> numbers;
 	Integer result = 0;
