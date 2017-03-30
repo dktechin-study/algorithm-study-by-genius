10b00eb4bc2465fd3d35b42309011bd3b45750b5
genius.choi <genius.choi@kakaocorp.com>
2017-03-30 17:01:29 +0900

    SafePassage

--- src/codingDojang/SafePassage.java
+++ src/codingDojang/SafePassage.java
@@ -1,7 +1,16 @@
 package codingDojang;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+* 주어진 입력을 오름차순으로 정렬하고 2개의 데크를 이용 하면 가능 할 듯
+* */
 public class SafePassage {
 	public static void main(String[] args) {
-
+	//	Integer[] a = Arrays.stream("1,2,3,4,5".split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()).toArray(new Integer[0]);
+	//	Integer[] b = Arrays.stream("1,2,3,4,5".split(",")).mapToInt(Integer::valueOf).boxed().toArray(Integer[]::new);
+		//System.out.println(Arrays.toString(Arrays.stream("1,2,3,4,5".split(",")).map(Integer::valueOf).collect(Collectors.toList()).toArray(new Integer[0])));
+		System.out.println(Arrays.toString(Arrays.stream("1,2,3,4,5".split(",")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new)));
 	}
 }
