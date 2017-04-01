13c9357a16d58cc14e239594147dc0c0002b226d
genius.choi <genius.choi@kakaocorp.com>
2017-04-02 01:41:45 +0900

    SafePassage

--- src/codingDojang/SafePassage.java
+++ src/codingDojang/SafePassage.java
@@ -1,16 +1,49 @@
 package codingDojang;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
+
+import static java.lang.System.in;
 
 /*
 * 주어진 입력을 오름차순으로 정렬하고 2개의 데크를 이용 하면 가능 할 듯
 * */
 public class SafePassage {
 	public static void main(String[] args) {
-	//	Integer[] a = Arrays.stream("1,2,3,4,5".split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()).toArray(new Integer[0]);
-	//	Integer[] b = Arrays.stream("1,2,3,4,5".split(",")).mapToInt(Integer::valueOf).boxed().toArray(Integer[]::new);
-		//System.out.println(Arrays.toString(Arrays.stream("1,2,3,4,5".split(",")).map(Integer::valueOf).collect(Collectors.toList()).toArray(new Integer[0])));
-		System.out.println(Arrays.toString(Arrays.stream("1,2,3,4,5".split(",")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new)));
+		Scanner sc = new Scanner(in);
+		int n = sc.nextInt();
+
+		List<Integer> d = new ArrayList<>();
+		List<Integer> e = new ArrayList();
+
+		for (int i = 0; i < n; i++) {
+			d.add(sc.nextInt());
+		}
+
+		d = d.stream().sorted().collect(Collectors.toList());
+
+		Integer k = 0;
+
+		while (true) {
+			Integer a, b, c;
+			a = d.remove(0);
+			b = d.remove(0);
+			e.add(a);
+			e.add(b);
+			k = k + (a > b ? a : b);
+
+			if (d.size() == 0) break;
+
+			d = d.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+			e = e.stream().sorted().collect(Collectors.toList());
+
+			c = e.remove(0);
+			d.add(c);
+			k = k + c;
+		}
+		System.out.println(k);
 	}
 }
