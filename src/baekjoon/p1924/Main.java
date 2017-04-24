303abe665b9ebd52bcb005a4510fce348fb7ff8f
genius.choi <genius.choi@kakaocorp.com>
2017-04-24 23:15:41 +0900

    baekjoon.p1924

--- src/baekjoon/p1924/Main.java
+++ src/baekjoon/p1924/Main.java
@@ -1,7 +1,22 @@
 package baekjoon.p1924;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		String[] week = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+		int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+
+		int t = n;
+
+		for (int i = 1; i < m; i++) {
+			t = t + months[i - 1];
+		}
+
+		System.out.println(week[t % 7]);
 	}
 }
