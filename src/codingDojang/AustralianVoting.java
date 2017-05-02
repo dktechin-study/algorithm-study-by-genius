4611fa0d8d661f8ec6b1a9869653aafd89a7f8de
genius.choi <genius.choi@kakaocorp.com>
2017-05-02 18:05:57 +0900

    AustralianVoting 진행중

--- src/codingDojang/AustralianVoting.java
+++ src/codingDojang/AustralianVoting.java
@@ -1,8 +1,50 @@
 package codingDojang;
 
+import javax.swing.text.html.parser.Entity;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class AustralianVoting {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		String path = AustralianVoting.class.getResource("").getPath();
+		Scanner sc = new Scanner(new File(path + "/AustralianVoting.txt"));
 
+		int n = Integer.valueOf(sc.nextLine());
+		String[] v = new String[n];
+		int[][] nn = new int[5][n];
+
+		for (int i = 0; i < n; i++) {
+			v[i] = sc.nextLine();
+		}
+
+		int k = 0;
+		while (sc.hasNextLine()) {
+			for (int i = 0; i < n; i++) {
+				nn[k][i] = sc.nextInt();
+			}
+			k++;
+		}
+
+		for (int i = 0; i < n; i++) {
+			List<Integer> list = new ArrayList<>();
+			for (int j = 0; j < 5; j++) {
+				list.add(nn[j][i]);
+			}
+			Map<Integer, Long> map = list.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+			Optional<Map.Entry<Integer, Long>> max = map.entrySet().stream().max((e1, e2) -> e1.getValue().compareTo(e2.getValue()));
+
+			if (max.get().getValue() > (k / 2)) {
+				System.out.println();
+				break;
+			}
+
+				System.out.println(map);
+			// 탈락후보제거
+		}
 	}
 }
