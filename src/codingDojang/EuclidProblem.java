974438fb083ef7814acc74d4886ea683323e0738
genius.choi <genius.choi@kakaocorp.com>
2017-05-17 18:21:15 +0900

    Euclid Problem

--- src/codingDojang/EuclidProblem.java
+++ src/codingDojang/EuclidProblem.java
@@ -1,9 +1,18 @@
 package codingDojang;
 
+import java.text.NumberFormat;
+
 public class EuclidProblem {
 
 	public static void main(String[] args) {
 		System.out.println(gcd(6, 4));
+
+		double aa  = 0.1233343;
+		double bb  = 1;
+		NumberFormat defaultFormat =  NumberFormat.getPercentInstance();
+		defaultFormat.setMinimumFractionDigits(2);
+		System.out.println(defaultFormat.format(aa));
+		System.out.println(defaultFormat.format(bb));
 	}
 
 	private static int gcd(int a, int b) {
